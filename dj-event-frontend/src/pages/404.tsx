1ba496af32f9d8bbd1e97a906907fa875730cb43
Huy Le <huyquoc.le@team.enouvo>
2023-08-12 12:09:39 +0700

    feat: add 404 page

---
+++ dj-event-frontend/src/pages/404.tsx
@@ -0,0 +1,18 @@
+import { FaExclamationTriangle } from 'react-icons/fa'
+import Link from 'next/link'
+import Layout from '@/components/Layout'
+import styles from '../styles/404.module.css'
+
+function NotFoundPage() {
+  return (
+    <Layout title='Page not found'>
+      <div className={styles.error}>
+        <h1><FaExclamationTriangle /> 404</h1>
+        <h4>Sorry, there is not thing here</h4>
+        <Link href='/'>Go Back</Link>
+      </div>
+    </Layout>
+  )
+}
+
+export default NotFoundPage
