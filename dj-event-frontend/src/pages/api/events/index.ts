601bd96174661fbfbaad4067210f1c48acda314d
Huy Le <huyquoc.le@team.enouvo>
2023-08-12 14:02:28 +0700

    feat: practice sever side redering

---
+++ dj-event-frontend/src/pages/api/events/index.ts
@@ -0,0 +1,14 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import events from "./data.json";
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  if (req.method === 'GET') {
+    res.status(200).json({ data: events });
+  }else {
+    res.setHeader('Allow', ['GET'])
+    res.status(405).json({message: `Method ${req.method} is not allow`})
+  }
+}
--- dj-event-frontend/src/pages/index.tsx
+++ dj-event-frontend/src/pages/index.tsx
@@ -1,12 +1,42 @@
 import { Inter } from 'next/font/google'
 import Layout from '@/components/Layout'
+import { API_URL } from '@/configs'
+import { GetServerSideProps } from 'next';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+type Event = {
+  id: string;
+  name: string;
+  slug: string;
+  venue: string;
+  address: string;
+  performers: string;
+  date: string;
+  time: string;
+  description: string;
+  image: string;
+};
+
+interface PropsHome {
+  events: Event[]
+}
+
+export default function Home({ events }: PropsHome) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   )
+}
+
+export const getServerSideProps: GetServerSideProps<{ events: Event[] }> = async () => {
+  const res = await fetch(`${API_URL}/api/events`)
+  const { data } = await res.json()
+
+  return {
+    props: {
+      events: data.events
+    }
+  }
 }
